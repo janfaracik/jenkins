a62e025ec5763161df59d8e00b96da30b95e709d
Jan Faracik <43062514+janfaracik@users.noreply.github.com>
2023-07-08 16:42:16 +0100

    push

--- core/src/main/java/jenkins/widgets/DescriptionWidget.java
+++ core/src/main/java/jenkins/widgets/DescriptionWidget.java
@@ -1,10 +1,32 @@
 package jenkins.widgets;
 
+import edu.umd.cs.findbugs.annotations.NonNull;
 import hudson.Extension;
+import hudson.model.View;
 import hudson.widgets.Widget;
+import java.util.Collection;
+import java.util.List;
 import org.jenkinsci.Symbol;
 
-@Extension(ordinal = 200) @Symbol("description")
 public class DescriptionWidget extends Widget {
 
+    @Extension()
+    @Symbol("description")
+    public static final class ViewFactoryImpl extends WidgetFactory<View, DescriptionWidget> {
+        @Override
+        public Class<View> type() {
+            return View.class;
+        }
+
+        @Override
+        public Class<DescriptionWidget> widgetType() {
+            return DescriptionWidget.class;
+        }
+
+        @NonNull
+        @Override
+        public Collection<DescriptionWidget> createFor(@NonNull View target) {
+            return List.of(new DescriptionWidget());
+        }
+    }
 }
