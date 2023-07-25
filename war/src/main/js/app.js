fd0316a69116f96d457c82ae4387ee55d74aee38
Jan Faracik <43062514+janfaracik@users.noreply.github.com>
2023-07-25 17:58:02 +0100

    Init

--- war/src/main/js/app.js
+++ war/src/main/js/app.js
@@ -5,6 +5,7 @@ import Tooltips from "@/components/tooltips";
 import StopButtonLink from "@/components/stop-button-link";
 import ConfirmationLink from "@/components/confirmation-link";
 import Dialogs from "@/components/dialogs";
+import Treeview from "@/components/treeview";
 
 Dropdowns.init();
 Notifications.init();
@@ -13,3 +14,4 @@ Tooltips.init();
 StopButtonLink.init();
 ConfirmationLink.init();
 Dialogs.init();
+Treeview.init();
