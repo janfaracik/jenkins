6e2190b27904f1840d426faff2f6c13db251b608
Jan Faracik <43062514+janfaracik@users.noreply.github.com>
2023-08-15 20:47:45 +0100

    Init

--- war/src/main/js/pages/manage-jenkins/index.js
+++ war/src/main/js/pages/manage-jenkins/index.js
@@ -11,3 +11,30 @@ searchBarInput.suggestions = function () {
     }))
     .filter((item) => !item.url.endsWith("#"));
 };
+
+const booter = document.querySelector(".booter");
+const img = document.querySelector(".booter img");
+
+document.addEventListener("mousemove", parallax);
+function parallax(event) {
+  this.querySelectorAll(".mouse").forEach((shift) => {
+    const position = shift.getAttribute("value");
+
+    const half = window.innerWidth / 2;
+    const posFromHalf = (half - event.pageX) * -1;
+    const offsetX = posFromHalf / half;
+
+
+    const halfY = window.innerHeight / 2;
+    const posYFromHalf = (halfY - event.pageY) * -1;
+    const offsetY = posYFromHalf / halfY;
+
+    const x = position * offsetX;
+    const y = position * offsetY;
+
+    shift.style.setProperty('--shadow-offset-x', x + 'px');
+    shift.style.setProperty('--shadow-offset-y', y + 'px');
+    shift.style.translate = `${x}px ${y}px`;
+    shift.style.rotate = `${x}deg`;
+  });
+}
