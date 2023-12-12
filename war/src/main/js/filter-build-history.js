d1fd7a9a93b241e93b18ed97af696bd32997d8b7
Jan Faracik <43062514+janfaracik@users.noreply.github.com>
2023-12-12 20:25:44 +0000

    Tidy up

--- war/src/main/js/filter-build-history.js
+++ war/src/main/js/filter-build-history.js
@@ -5,10 +5,12 @@ const pageSearchInput = buildHistoryPage.querySelector(
   "input",
 );
 const ajaxUrl = buildHistoryPage.getAttribute("page-ajax");
-const xander = document.querySelector("#xander");
-const noBuilds = document.querySelector("#no-builds")
+const card = document.querySelector("#jenkins-builds");
+const xander = card.querySelector("#xander");
+const container = card.querySelector(".app-builds-container");
+const noBuilds = card.querySelector("#no-builds")
 
-const updateBuildsRefreshInterval = 500000;
+const updateBuildsRefreshInterval = 5000;
 
 function loadPage() {
   const params = {
@@ -18,6 +20,8 @@ function loadPage() {
   fetch(ajaxUrl + toQueryString(params)).then((rsp) => {
     if (rsp.ok) {
       rsp.text().then((responseText) => {
+        container.classList.remove("app-builds-container--loading");
+
         if (responseText.trim() === "") {
           xander.innerHTML = "";
           noBuilds.style.display = "block";
@@ -39,13 +43,19 @@ const handleFilter = function () {
 const debouncedFilter = debounce(handleFilter, 300);
 
 setInterval(() => {
-  loadPage({})
+  loadPage()
 }, updateBuildsRefreshInterval)
 
 document.addEventListener("DOMContentLoaded", function () {
   pageSearchInput.addEventListener("input", function () {
+    container.classList.add("app-builds-container--loading");
     debouncedFilter();
   });
 
   loadPage({});
 });
+
+document.querySelectorAll(".task-link").forEach((taskLink) =>
+  taskLink.addEventListener('click', () => {
+    loadPage();
+}))
