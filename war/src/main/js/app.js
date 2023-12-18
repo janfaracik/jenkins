934dbfa384d203a854b3df4ba5e5a6a1076180bf
Jan Faracik <43062514+janfaracik@users.noreply.github.com>
2023-12-18 20:56:27 +0000

    Init

--- war/src/main/js/app.js
+++ war/src/main/js/app.js
@@ -13,3 +13,11 @@ Tooltips.init();
 StopButtonLink.init();
 ConfirmationLink.init();
 Dialogs.init();
+
+// Bootstrap deprecations
+
+const deprecatedClasses = ['.container', '.container-fluid', '.row', '.col-md-24'];
+const matchedClasses = document.querySelectorAll(deprecatedClasses.join(', '));
+if (matchedClasses.length > 0) {
+  console.warn('This page contains several deprecated classes - these should be updated', matchedClasses)
+}
