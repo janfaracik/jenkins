7b3b6e2d225441f29a25f1ff4df725b8ef79ce6c
Jan Faracik <43062514+janfaracik@users.noreply.github.com>
2024-01-20 11:21:38 +0000

    More

--- core/src/main/java/jenkins/views/DensityProperty.java
+++ core/src/main/java/jenkins/views/DensityProperty.java
@@ -3,12 +3,12 @@ package jenkins.views;
 import edu.umd.cs.findbugs.annotations.NonNull;
 import edu.umd.cs.findbugs.annotations.Nullable;
 import hudson.Extension;
-import hudson.model.Descriptor;
-import hudson.model.User;
-import hudson.model.UserProperty;
-import hudson.model.UserPropertyDescriptor;
+import hudson.model.*;
+import hudson.util.ListBoxModel;
+import java.util.List;
 import net.sf.json.JSONObject;
 import org.jenkinsci.Symbol;
+import org.kohsuke.stapler.AncestorInPath;
 import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.StaplerRequest;
 
@@ -26,14 +26,26 @@ public class DensityProperty extends UserProperty {
         return density;
     }
 
-    public static DensityType getDensityValueForCurrentUser() {
-        User user = User.current();
-
+    public static DensityType getDensityValueForUser(User user) {
         if (user == null) {
             return DensityType.DEFAULT;
         }
 
-        return user.getProperty(DensityProperty.class).getDensity();
+        DensityType density = user.getProperty(DensityProperty.class).getDensity();
+
+        if (density == null) {
+            return DensityType.DEFAULT;
+        }
+
+        return density;
+    }
+
+    public static DensityType getDensityValueForCurrentUser() {
+        return getDensityValueForUser(User.current());
+    }
+
+    public static List<DensityType> getDensities() {
+        return List.of(DensityType.values());
     }
 
     public enum DensityType {
@@ -59,6 +71,15 @@ public class DensityProperty extends UserProperty {
         @Override
         public UserProperty newInstance(@Nullable StaplerRequest req, @NonNull JSONObject formData) throws Descriptor.FormException {
             return new DensityProperty(DensityType.valueOf(formData.getString("density")));
+        }
+
+        public ListBoxModel doFillDensityItems(@AncestorInPath User user) {
+            DensityType currentDensity = user != null ? getDensityValueForUser(user) : getDensityValueForCurrentUser();
+            ListBoxModel items = new ListBoxModel();
+            for (DensityType id : getDensities()) {
+                items.add(new ListBoxModel.Option(id.name().toLowerCase(), id.toString(), currentDensity == id));
+            }
+            return items;
         }
     }
 }
