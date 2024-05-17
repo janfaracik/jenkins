9ac78a616c71ecaa374961d42d09ee8793405574
Jan Faracik <43062514+janfaracik@users.noreply.github.com>
2024-05-17 15:04:46 +0100

    Add locale/js times

--- war/src/main/js/pages/job/index.js
+++ war/src/main/js/pages/job/index.js
@@ -15,3 +15,53 @@ document.getElementById('flipButton').addEventListener('click', function() {
     back.setAttribute('aria-hidden', 'false');
   }
 });
+
+document.querySelectorAll('[data-initial-time]').forEach(element => {
+  const locale = document.querySelector('html').dataset.locale;
+  const template = element.getAttribute('data-time-template');
+  const startTime = new Date(Number(element.getAttribute('data-initial-time')));
+
+  const formatElapsedTime = (time, unit) => {
+    return Intl.NumberFormat(locale, {
+      style: "unit",
+      unit: unit,
+      unitDisplay: "short"
+    }).format(time);
+  };
+
+  function updateElapsedTime() {
+    const now = new Date();
+    const elapsed = now - startTime;
+    const seconds = Math.floor((elapsed / 1000) % 60);
+    const minutes = Math.floor((elapsed / (1000 * 60)) % 60);
+    const hours = Math.floor((elapsed / (1000 * 60 * 60)) % 24);
+    const days = Math.floor((elapsed / (1000 * 60 * 60 * 24)) % 30);
+    const months = Math.floor((elapsed / (1000 * 60 * 60 * 24 * 30)) % 12);
+    const years = Math.floor(elapsed / (1000 * 60 * 60 * 24 * 365));
+
+    let elapsedTimeString = '';
+    if (years > 0) {
+      elapsedTimeString += formatElapsedTime(years, 'year') + ' ';
+    }
+    if (months > 0) {
+      elapsedTimeString += formatElapsedTime(months, 'month') + ' ';
+    }
+    if (days > 0) {
+      elapsedTimeString += formatElapsedTime(days, 'day') + ' ';
+    }
+    if (hours > 0 && days < 10 && months === 0 && years === 0) {
+      elapsedTimeString += formatElapsedTime(hours, 'hour') + ' ';
+    }
+    if (minutes > 0 && days === 0) {
+      elapsedTimeString += formatElapsedTime(minutes, 'minute') + ' ';
+    }
+    if (hours === 0 && minutes < 10) {
+      elapsedTimeString += formatElapsedTime(seconds, 'second');
+    }
+
+    element.textContent = template.replace("####", elapsedTimeString);
+  }
+
+  updateElapsedTime();
+  setInterval(updateElapsedTime, 1000);
+})
