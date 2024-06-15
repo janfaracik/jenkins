5c04fc85c69b7b7f62fd24a55c43a21205ea591a
Jan Faracik <43062514+janfaracik@users.noreply.github.com>
2024-06-15 14:54:31 +0100

    Update index.js

--- war/src/main/js/pages/project/builds/index.js
+++ war/src/main/js/pages/project/builds/index.js
@@ -60,7 +60,7 @@ window.buildTimeTrend_displayBuilds = function (data) {
         ${distributedBuildColumn}
         <td>
           <button data-href="${item.number}/" class="jenkins-button jenkins-button--tertiary jenkins-jumplist-link">
-            <svg xmlns="http://www.w3.org/2000/svg" class="ionicon" viewBox="0 0 512 512"><title>Chevron Down</title><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="48" d="M112 184l144 144 144-144"/></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="48" d="M112 184l144 144 144-144"/></svg>
           </button>
         </td>
       </tr>`);
