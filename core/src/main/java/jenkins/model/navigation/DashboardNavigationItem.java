eb319f2394ef7555b2c035ab595b3fef3417254b
Jan Faracik <43062514+janfaracik@users.noreply.github.com>
2024-12-23 14:31:00 +0000

    push

--- core/src/main/java/jenkins/model/navigation/DashboardNavigationItem.java
+++ core/src/main/java/jenkins/model/navigation/DashboardNavigationItem.java
@@ -1,7 +1,7 @@
 package jenkins.model.navigation;
 
 import org.kohsuke.stapler.Stapler;
-import org.kohsuke.stapler.StaplerRequest;
+import org.kohsuke.stapler.StaplerRequest2;
 
 public class DashboardNavigationItem implements NavigationItem {
 
@@ -17,8 +17,8 @@ public class DashboardNavigationItem implements NavigationItem {
 
     @Override
     public String getUrl() {
-        StaplerRequest currentRequest = Stapler.getCurrentRequest();
-        currentRequest.getWebApp().getDispatchValidator().allowDispatch(currentRequest, Stapler.getCurrentResponse());
+        StaplerRequest2 currentRequest = Stapler.getCurrentRequest2();
+        currentRequest.getWebApp().getDispatchValidator().allowDispatch(currentRequest, Stapler.getCurrentResponse2());
 
         return currentRequest.getContextPath();
     }
