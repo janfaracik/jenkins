01c8ea467650c00b1819b022cb17e2e8ae86df58
Jan Faracik <43062514+janfaracik@users.noreply.github.com>
2025-01-18 09:56:29 +0000

    Fix nodes page

--- src/main/js/pages/computer-set/index.js
+++ src/main/js/pages/computer-set/index.js
@@ -1,16 +1,22 @@
 import { createElementFromHtml } from "@/util/dom";
+import behaviorShim from "@/util/behavior-shim";
 
-document
-  .querySelector("#button-computer-icon-legend")
-  .addEventListener("click", () => {
-    const template = document.querySelector("#template-computer-icon-legend");
-    const title = template.getAttribute("data-title");
-    const content = createElementFromHtml(
-      "<div>" + template.innerHTML + "</div>",
-    );
+behaviorShim.specify(
+  "#button-computer-icon-legend",
+  "icon-legend",
+  999,
+  (button) => {
+    button.addEventListener("click", () => {
+      const template = document.querySelector("#template-computer-icon-legend");
+      const title = template.getAttribute("data-title");
+      const content = createElementFromHtml(
+        "<div>" + template.innerHTML + "</div>",
+      );
 
-    dialog.modal(content, {
-      maxWidth: "550px",
-      title: title,
+      dialog.modal(content, {
+        maxWidth: "550px",
+        title: title,
+      });
     });
-  });
+  },
+);
