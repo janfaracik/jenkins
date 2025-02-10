f175ed1cbd6a91adc478694c1c139cefd611c566
Jan Faracik <43062514+janfaracik@users.noreply.github.com>
2025-02-10 19:05:03 +0000

    WB

--- src/main/js/components/side-panel/index.js
+++ src/main/js/components/side-panel/index.js
@@ -1,19 +1,45 @@
 import Utils from "@/components/dropdowns/utils";
 import { createElementFromHtml } from "@/util/dom";
 
-function init() {
-  const tasks = document.querySelector("#side-panel #tasks")
-  const sidepanelMenu = document.querySelector("#side-panel-menu");
+const BREAKPOINT = "768";
+const SIDEPANEL = document.querySelector("#side-panel");
+const TASKS = SIDEPANEL.querySelector("#tasks");
+const SIDEPANEL_MENU_BUTTON = document.querySelector("#side-panel-menu");
 
-  if (!tasks) {
-    sidepanelMenu.remove();
+let isAboveBreakpoint = window.innerWidth > BREAKPOINT;
+
+function init() {
+  window.addEventListener("resize", () => onWidthChange(evaluate));
+  evaluate();
+}
+
+function evaluate() {
+  // Reset state if need be
+  if (!TASKS.closest("#side-panel")) {
+    SIDEPANEL.appendChild(TASKS);
   }
 
-  Utils.generateDropdown(sidepanelMenu, (instance) => {
-    const parent = createElementFromHtml(`<div class="jenkins-dropdown app-side-panel-menu__dropdown"></div>`)
-    parent.appendChild(tasks);
-    instance.setContent(parent);
-  })
+  // Generate the dropdown if we're below the breakpoint
+  if (!isAboveBreakpoint) {
+    Utils.generateDropdown(SIDEPANEL_MENU_BUTTON, (instance) => {
+      const parent = createElementFromHtml(
+        `<div class="jenkins-dropdown app-side-panel-menu__dropdown"></div>`,
+      );
+      parent.appendChild(TASKS);
+      instance.setContent(parent);
+    });
+  }
+}
+
+function onWidthChange(callback) {
+  const currentWidth = window.innerWidth;
+  const isNowAboveBreakpoint = currentWidth > BREAKPOINT;
+
+  // Trigger only if the state changes
+  if (isNowAboveBreakpoint !== isAboveBreakpoint) {
+    callback();
+    isAboveBreakpoint = isNowAboveBreakpoint;
+  }
 }
 
 export default { init };
