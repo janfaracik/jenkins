4dd6af35921c1961bfc0e50054bda493fdd660b8
Jan Faracik <43062514+janfaracik@users.noreply.github.com>
2025-08-02 09:31:05 +0100

    Update main.groovy

--- core/src/main/resources/hudson/model/View/main.groovy
+++ core/src/main/resources/hudson/model/View/main.groovy
@@ -5,4 +5,27 @@ import hudson.model.MyViewsProperty
 t=namespace(lib.JenkinsTagLib)
 st=namespace("jelly:stapler")
 
-include(my,"noJob.jelly")
+if (items == null) {
+    p(_('broken'))
+} else if (items.isEmpty()) {
+    if (app.items.size() != 0) {
+        set("views",my.owner.views)
+        set("currentView",my)
+        if (my.owner.class == MyViewsProperty.class) {
+            include(my.owner?.viewsTabBar, "viewTabs")
+        } else {
+            include(my.owner.userViewsTabBar, "viewTabs")
+        }
+    }
+    include(my,"noJob.jelly")
+} else {
+    t.projectView(jobs: items, showViewTabs: true, columnExtensions: my.columns, indenter: my.indenter, itemGroup: my.owner.itemGroup) {
+        set("views",my.owner.views)
+        set("currentView",my)
+        if (my.owner.class == MyViewsProperty.class) {
+            include(my.owner?.viewsTabBar, "viewTabs")
+        } else {
+            include(my.owner.userViewsTabBar,"viewTabs")
+        }
+    }
+}
