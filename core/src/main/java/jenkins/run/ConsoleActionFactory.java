69828822b2ad536f1badf01b74d800db3afc76cc
Jan Faracik <43062514+janfaracik@users.noreply.github.com>
2025-09-09 01:27:17 +0100

    Update ConsoleActionFactory.java

--- core/src/main/java/jenkins/run/ConsoleActionFactory.java
+++ core/src/main/java/jenkins/run/ConsoleActionFactory.java
@@ -2,9 +2,11 @@ package jenkins.run;
 
 import edu.umd.cs.findbugs.annotations.NonNull;
 import hudson.Extension;
+import hudson.Functions;
 import hudson.model.Run;
 import java.util.Collection;
 import java.util.Collections;
+import jenkins.console.DefaultConsoleUrlProvider;
 import jenkins.model.TransientActionFactory;
 
 @Extension(ordinal = Integer.MAX_VALUE - 1)
@@ -18,6 +20,12 @@ public class ConsoleActionFactory extends TransientActionFactory<Run> {
     @NonNull
     @Override
     public Collection<? extends RunTab> createFor(@NonNull Run target) {
+        var consoleProvider = Functions.getConsoleProviderFor(target);
+
+        if (!consoleProvider.getClass().equals(DefaultConsoleUrlProvider.class)) {
+            return Collections.emptySet();
+        }
+
         return Collections.singleton(new ConsoleAction(target));
     }
 }
