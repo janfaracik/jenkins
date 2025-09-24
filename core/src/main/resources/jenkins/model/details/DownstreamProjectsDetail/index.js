1c4bd11754f2984f4659796713872db9beb5b0bb
Jan Faracik <43062514+janfaracik@users.noreply.github.com>
2025-09-24 11:00:47 +0100

    Push

--- core/src/main/resources/jenkins/model/details/DownstreamProjectsDetail/index.js
+++ core/src/main/resources/jenkins/model/details/DownstreamProjectsDetail/index.js
@@ -1,9 +1,18 @@
 document.querySelector("#downstream-projects").addEventListener("click", () => {
-  const template = document.querySelector("#downstream-projects-template");
-  const title = template.dataset.title;
-  const content = template.content.firstElementChild.cloneNode(true);
-  dialog.modal(content, {
-    maxWidth: "550px",
-    title: title,
-  });
+  function render() {
+    const template = document.querySelector("#downstream-projects-template");
+    const title = template.dataset.title;
+    const content = template.content.firstElementChild.cloneNode(true);
+    dialog.modal(content, {
+      maxWidth: "550px",
+      title: title,
+    });
+  }
+
+  if (document.querySelector("#downstream-projects-template")) {
+    render();
+    return;
+  }
+
+  renderOnDemand(document.querySelector(".downstream-projects-template"), render);
 });
