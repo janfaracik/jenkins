170e2f743808c31b89ca6da18803b3e9295a3b56
Jan Faracik <43062514+janfaracik@users.noreply.github.com>
2025-12-04 10:49:56 +0000

    Tidy UI

--- src/main/js/pages/cloud-set/index.js
+++ src/main/js/pages/cloud-set/index.js
@@ -1,9 +1,15 @@
 import { registerSortableDragDrop } from "@/sortable-drag-drop";
 
 document.addEventListener("DOMContentLoaded", function () {
-  document.querySelectorAll(".with-drag-drop").forEach((table) =>
-    registerSortableDragDrop(table, function () {
-      document.getElementById("saveButton").classList.remove("jenkins-hidden");
-    }),
-  );
+  const bottomStickerShadow = document.querySelector(".jenkins-bottom-app-bar__shadow");
+  const bottomSticker = document.querySelector("#bottom-sticker");
+
+  bottomStickerShadow.classList.add("jenkins-hidden");
+  bottomSticker.classList.add("jenkins-hidden");
+
+  const items = document.querySelector(".with-drag-drop");
+  registerSortableDragDrop(items, function () {
+    bottomStickerShadow.classList.remove("jenkins-hidden");
+    bottomSticker.classList.remove("jenkins-hidden");
+  })
 });
