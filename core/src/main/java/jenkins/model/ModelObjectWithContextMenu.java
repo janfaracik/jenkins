a8649fd962fb72d1abf2b78ef200066237239895
Jan Faracik <43062514+janfaracik@users.noreply.github.com>
2026-03-25 19:25:23 +0000

    Fix test

--- core/src/main/java/jenkins/model/ModelObjectWithContextMenu.java
+++ core/src/main/java/jenkins/model/ModelObjectWithContextMenu.java
@@ -291,7 +291,7 @@ public interface ModelObjectWithContextMenu extends ModelObject {
 
         public ContextMenu from(ModelObjectWithContextMenu self, StaplerRequest2 request, StaplerResponse2 response, String view) throws JellyException, IOException {
             // Only Jobs and Runs support getAppBarActions currently
-            if (self instanceof Run || self instanceof Job<?,?>) {
+            if (self instanceof Run || self instanceof Job<?, ?>) {
                 this.addAll(((Actionable) self).getAppBarActions().stream()
                         .filter(action ->
                                 action.getIconFileName() != null
