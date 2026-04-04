4c4d54b6e3a1b038f6c11f4e8c01d54503726b1f
Jan Faracik <43062514+janfaracik@users.noreply.github.com>
2026-04-04 19:14:21 +0100

    Refine

--- src/main/js/pages/computer-set/index.js
+++ src/main/js/pages/computer-set/index.js
@@ -1,16 +1,23 @@
 import { createElementFromHtml } from "@/util/dom";
+import behaviorShim from "@/util/behavior-shim";
 
-document
-  .querySelector("#button-computer-icon-legend")
-  .addEventListener("click", () => {
-    const template = document.querySelector("#template-computer-icon-legend");
-    const title = template.getAttribute("data-title");
-    const content = createElementFromHtml(
-      "<div>" + template.innerHTML + "</div>",
-    );
+behaviorShim.specify(
+  "#button-computer-icon-legend",
+  "computer-icon-legend",
+  999,
+  (button) => {
+    button.addEventListener("click", () => {
+      console.log("clicked");
+      const template = document.querySelector("#template-computer-icon-legend");
+      const title = template.getAttribute("data-title");
+      const content = createElementFromHtml(
+        "<div>" + template.innerHTML + "</div>",
+      );
 
-    dialog.modal(content, {
-      maxWidth: "550px",
-      title: title,
+      dialog.modal(content, {
+        maxWidth: "550px",
+        title: title,
+      });
     });
-  });
+  },
+);
