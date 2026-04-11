e9f97e403207975f1d7317d5859ee8cd24a49594
Jan Faracik <43062514+janfaracik@users.noreply.github.com>
2026-04-11 11:06:01 +0100

    Init

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -8,6 +8,7 @@ import StopButtonLink from "@/components/stop-button-link";
 import ConfirmationLink from "@/components/confirmation-link";
 import Dialogs from "@/components/dialogs";
 import Defer from "@/components/defer";
+import behaviorShim from "@/util/behavior-shim";
 
 AppBar.init();
 Dropdowns.init();
@@ -19,3 +20,49 @@ Tooltips.init();
 StopButtonLink.init();
 ConfirmationLink.init();
 Dialogs.init();
+
+behaviorShim.specify("[data-type='main-panel-thing']", '', 999, (mainPanelThing) => {
+  const tabs = document.querySelectorAll(".app-build-tabs a");
+  const interceptUrl = mainPanelThing.dataset.interceptUrl;
+
+  behaviorShim.specify("a", "link", 999, (link) => {
+    if (link.href.includes(interceptUrl)) {
+      link.addEventListener("click", (e) => {
+        e.preventDefault();
+
+        // do the thing
+        loadPage(mainPanelThing, link, tabs);
+      })
+    }
+  });
+
+  actuallyLoadPage(mainPanelThing, window.location.href);
+});
+
+function loadPage(mainPanelThing, link, tabs) {
+  // Do some magic
+  tabs.forEach((tab) => {
+    tab.classList.add("jenkins-button--tertiary");
+  });
+
+  link.classList.remove("jenkins-button--tertiary");
+
+  actuallyLoadPage(mainPanelThing, link.href);
+}
+
+function actuallyLoadPage(mainPanelThing, href) {
+  mainPanelThing.innerHTML = "";
+
+  fetch(href, {
+    method: "GET",
+    headers: crumb.wrap({
+      "X-Content-Only": true,
+    }),
+  }).then((rsp) => {
+    if (rsp.ok) {
+      rsp.text().then((responseText) => {
+        mainPanelThing.innerHTML = responseText;
+      });
+    }
+  });
+}
